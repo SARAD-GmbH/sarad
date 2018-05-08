195a0706292229efea816291ac1594ea7674f8b8
Michael Strey <strey@sarad.de>
2018-05-08 14:47:09 +0200

    Introduce Click with Virtualenv and Setuptools

--- data_collector.py
+++ data_collector.py
@@ -4,26 +4,33 @@ Made to be a data source for Zabbix agent."""
 
 import SarI
 import click
-# Test environment
-if __name__=='__main__':
 
-    def print_dacm_value(dacm_value):
-        print("ComponentName: " + dacm_value['component_name'])
-        print("ValueName: " + dacm_value['value_name'])
-        print(DacmInst.item_names[dacm_value['item_id']] + \
-              ": " + dacm_value['result'])
-        if dacm_value['datetime'] is not None:
-            print("DateTime: " + dacm_value['datetime'].strftime("%c"))
-        print("GPS: " + dacm_value['gps'])
+@click.command()
+@click.option('--count', default=1, help='Number of greetings.')
+@click.option('--name', prompt='Your name',
+              help='The person to greet.')
+def cli(count, name):
+    """Simple program that greets NAME for a total of COUNT times."""
+    for x in range(count):
+        click.echo('Hello %s!' % name)
 
-    def print_radon_scout_value(radon_scout_value):
-        print(repr(("Radon: " + str(radon_scout_value['radon']) + " Bq/m³")))
-        if radon_scout_value['datetime'] is not None:
-            print("DateTime: " + radon_scout_value['datetime'].strftime("%c"))
+def print_dacm_value(dacm_value):
+    print("ComponentName: " + dacm_value['component_name'])
+    print("ValueName: " + dacm_value['value_name'])
+    print(DacmInst.item_names[dacm_value['item_id']] + \
+          ": " + dacm_value['result'])
+    if dacm_value['datetime'] is not None:
+        print("DateTime: " + dacm_value['datetime'].strftime("%c"))
+    print("GPS: " + dacm_value['gps'])
 
-    mycluster = SarI.SaradCluster()
-    for connected_instrument in mycluster.connected_instruments:
-        print(connected_instrument)
-        print()
+def print_radon_scout_value(radon_scout_value):
+    print(repr(("Radon: " + str(radon_scout_value['radon']) + " Bq/m³")))
+    if radon_scout_value['datetime'] is not None:
+        print("DateTime: " + radon_scout_value['datetime'].strftime("%c"))
 
-    print(mycluster.connected_instruments[0].get_recent_value(0,0,0)['result_value'])
+# mycluster = SarI.SaradCluster()
+# for connected_instrument in mycluster.connected_instruments:
+#     print(connected_instrument)
+#     print()
+
+# print(mycluster.connected_instruments[0].get_recent_value(0,0,0)['result_value'])
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='data_collector',
+    version='0.1',
+    py_modules=['data_collector', 'SarI'],
+    install_requires=[
+        'Click', 'pyserial'
+    ],
+    entry_points='''
+        [console_scripts]
+        data_collector=data_collector:cli
+    ''',
+)

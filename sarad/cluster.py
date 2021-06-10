ff417bbb333d692f13b21e3eff00713b94e4b59f
Michael Strey <mstrey@strey.biz>
2021-06-10 16:24:40 +0200

    !: move creation of mycluster from module to main
    ~: Black autoformatting

--- sarad/cluster.py
+++ sarad/cluster.py
@@ -213,13 +213,11 @@ class SaradCluster(Generic[SI]):
         self.__start_time = start_time
 
 
-# * Initialize mycluster as a singleton # TODO: In Hauptprogramm übernehmen
-#mycluster: SaradCluster = SaradCluster()
-#mycluster.update_connected_instruments()
-#logger.debug(mycluster.__dict__)
-
 # * Test environment:
 if __name__ == "__main__":
+    mycluster: SaradCluster = SaradCluster()
+    mycluster.update_connected_instruments()
+    logger.debug(mycluster.__dict__)
 
     for connected_instrument in mycluster:
         print(connected_instrument)

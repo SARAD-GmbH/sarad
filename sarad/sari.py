f4fc07ae799eb964b053f18ebc2e947b00e722bb
Michael Strey <mstrey@strey.biz>
2021-06-11 12:13:54 +0200

    !: TypeError in Python 3.8 under Windows

--- sarad/sari.py
+++ sarad/sari.py
@@ -7,10 +7,10 @@ import logging
 import os
 import struct
 import time
-from collections.abc import Collection, Iterator
 from datetime import datetime, timedelta
 from enum import Enum
-from typing import Any, Dict, Generic, List, Optional, TypedDict, TypeVar
+from typing import (Any, Collection, Dict, Generic, Iterator, List, Optional,
+                    TypedDict, TypeVar)
 
 import yaml
 from BitVector import BitVector  # type: ignore

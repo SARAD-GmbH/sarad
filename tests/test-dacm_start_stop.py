eb2a43c3576c9820998c655c41080206d9287875
Michael Strey <strey@sarad.de>
2024-04-08 11:41:54 +0200

    ~: Pylint

--- tests/test-dacm_start_stop.py
+++ tests/test-dacm_start_stop.py
@@ -21,39 +21,42 @@ COLOR = {
 
 
 def now():
+    """Formatting of logging times"""
     return datetime.now().strftime("%M:%S:%f")
 
 
 def comment(text):
+    """Formatting of comments"""
     print(f"{now()}  " + text)
 
 
 def heading(text):
+    """Formatting of headings"""
     print(f"\n{COLOR['HEADER']}{text}{COLOR['ENDC']}\n")
 
 
 my_cluster = SaradCluster()
 my_cluster.update_connected_instruments()
 my_instr = my_cluster.connected_instruments[0]
-no_of_attempts = 5
-heading(f"1st Test -- {no_of_attempts} attempts of Start/Stop")
+NO_OF_ATTEMPTS = 5
+heading(f"1st Test -- {NO_OF_ATTEMPTS} attempts of Start/Stop")
 for i in range(5):
     comment(f"Attempt {i + 1}: trying to start")
     my_instr.start_cycle(2)
     comment(f"Attempt {i + 1}: trying to stop")
     my_instr.stop_cycle()
 
-comment(f"Finally trying to start. You should hear the starting pump.")
+comment("Finally trying to start. You should hear the starting pump.")
 my_instr.start_cycle(2)
 
 sleep(15)
 comment("Finally trying to stop")
 my_instr.stop_cycle()
 
-heading(f"2nd Test -- {no_of_attempts} attempts of Start/Stop/Start")
-for i in range(no_of_attempts):
+heading(f"2nd Test -- {NO_OF_ATTEMPTS} attempts of Start/Stop/Start")
+for i in range(NO_OF_ATTEMPTS):
     if i:
-        comment(f"In the next attempt to start we are expecting an error message.")
+        comment("In the next attempt to start we are expecting an error message.")
     comment(f"Attempt {i + 1}: trying to start")
     my_instr.start_cycle(2)
     comment(f"Attempt {i + 1}: trying to stop")
@@ -64,15 +67,15 @@ for i in range(no_of_attempts):
 comment("Trying to stop")
 my_instr.stop_cycle()
 
-comment(f"Finally trying to start. You should hear the starting pump.")
+comment("Finally trying to start. You should hear the starting pump.")
 my_instr.start_cycle(2)
 
 sleep(15)
 comment("Finally trying to stop")
 my_instr.stop_cycle()
 
-heading(f"3rd Test -- {no_of_attempts} attempts of Stop/Start/Stop")
-for i in range(no_of_attempts):
+heading(f"3rd Test -- {NO_OF_ATTEMPTS} attempts of Stop/Start/Stop")
+for i in range(NO_OF_ATTEMPTS):
     comment(f"Attempt {i + 1}: trying to stop")
     my_instr.stop_cycle()
     comment(f"Attempt {i + 1}: trying to start")
@@ -80,11 +83,12 @@ for i in range(no_of_attempts):
     comment(f"Attempt {i + 1}: trying to stop")
     my_instr.stop_cycle()
 
-comment(f"Finally trying to start. You should hear the starting pump.")
+comment("Finally trying to start. You should hear the starting pump.")
 my_instr.start_cycle(2)
 
-heading(f"4th Test -- {no_of_attempts} attempts of Stop/Start/Stop/Start")
-for i in range(no_of_attempts):
+NO_OF_ATTEMPTS = 100
+heading(f"4th Test -- {NO_OF_ATTEMPTS} attempts of Stop/Start/Stop/Start")
+for i in range(NO_OF_ATTEMPTS):
     comment(f"Attempt {i + 1}: trying to stop")
     my_instr.stop_cycle()
     comment(f"Attempt {i + 1}: trying to start")
@@ -98,11 +102,11 @@ sleep(15)
 comment("Finally trying to stop")
 my_instr.stop_cycle()
 
-no_of_attempts = 100
-heading(f"5th Test -- {no_of_attempts} attempts of Start/Stop/Start")
-for i in range(no_of_attempts):
+NO_OF_ATTEMPTS = 100
+heading(f"5th Test -- {NO_OF_ATTEMPTS} attempts of Start/Stop/Start")
+for i in range(NO_OF_ATTEMPTS):
     if i:
-        comment(f"In the next attempt to start we are expecting an error message.")
+        comment("In the next attempt to start we are expecting an error message.")
     comment(f"Attempt {i + 1}: trying to start")
     my_instr.start_cycle(2)
     comment(f"Attempt {i + 1}: trying to stop")
@@ -113,7 +117,7 @@ for i in range(no_of_attempts):
 comment("Trying to stop")
 my_instr.stop_cycle()
 
-comment(f"Finally trying to start. You should hear the starting pump.")
+comment("Finally trying to start. You should hear the starting pump.")
 my_instr.start_cycle(2)
 
 sleep(10)

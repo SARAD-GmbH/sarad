2597772b42f281f1976f42aa069f2ed96f5d3b0a
Michael Strey <strey@sarad.de>
2024-04-10 16:20:09 +0200

    !: typo

--- src/sarad/mapping.py
+++ src/sarad/mapping.py
@@ -5,4 +5,4 @@ from sarad.doseman import DosemanInst
 from sarad.network import NetworkInst
 from sarad.radonscout import RscInst
 
-id_family_mapping = {1: DosemanInst(), 2: RscInst(), 4: NetworkInst, 5: DacmInst()}
+id_family_mapping = {1: DosemanInst(), 2: RscInst(), 4: NetworkInst(), 5: DacmInst()}
